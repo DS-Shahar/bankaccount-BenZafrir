cda3298f9b0d6c5a0dd67650c72c0e5217e53187
BenZafrir <zafrir.ben@gmail.com>
2024-09-21 20:55:54 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         BankAccount acct1 = new BankAccount(1000, -1000, "Lisa");
         BankAccount acct_target = new BankAccount(1000, -1000, "Bob");
         
         acct_target.state_balance1(acct_target);
-    	  acct1.state_balance1(acct1);
+    	acct1.state_balance1(acct1);
     	
-    	  acct1.transfer(acct_target, 500, acct1);
+    	acct1.transfer(acct_target, 500, acct1);
     	
         acct_target.state_balance1(acct_target);
-    	  acct1.state_balance1(acct1);
+    	acct1.state_balance1(acct1);
     	
-    	  acct1.transfer(acct_target, 1600, acct1);
-  }
+    	acct1.transfer(acct_target, 1600, acct1);
+    }
 }
